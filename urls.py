b6d49d08bba3742acbca62af1589b26473a2b564
Drabenq <163213167+Drabenq@users.noreply.github.com>
2024-06-21 15:48:59 -0300

    Add files via upload
    Commit de Login/Logout

---
+++ urls.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.urls import include,path
+from . import views
+
+urlpatterns = [
+    path('', views.index_page, name='index-page'),
+    path('login/', views.login_page, name='login'),
+    path('home/', views.home, name='home'),
+    path('buscar/', views.search, name='buscar'),
+
+    path('favourites/', views.getAllFavouritesByUser, name='favoritos'),
+    path('favourites/add/', views.saveFavourite, name='agregar-favorito'),
+    path('favourites/delete/', views.deleteFavourite, name='borrar-favorito'),
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('exit/', views.exit, name='exit'),
+]
